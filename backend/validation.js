ee50b09fc91c7e3aeb651c6e4645167156b36dd8
pkhlopiachyi <khlpavlo@gmail.com>
2020-11-02 17:26:18 +0200

    Fix: add valiadtion for release year

--- backend/validation.js
+++ backend/validation.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 const createFilmValidation = data => {
     const schema = Joi.object({
         title: Joi.string().min(2).max(255),
-        release_year: Joi.number().min(1895),
+        release_year: Joi.number().min(1850).max(2020),
         format: Joi.string(),
         stars: Joi.string(),
         image_link: Joi.string().optional().allow(''),
