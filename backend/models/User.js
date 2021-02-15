8993e9b1d13b565526392431e7138e5e9ba20b17
pkhlopiachyi <khlpavlo@gmail.com>
2021-02-16 00:04:03 +0200

    Feature: add User model

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 4,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 4,
+        max: 255
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 255
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
